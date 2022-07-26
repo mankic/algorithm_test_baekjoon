8b3c9773cb310947fd56e24f864a986a5d2a87b0
mankic <miic0803@gmail.com>
2022-07-27 01:06:17 +0900

    수정 - 학생 점수 입력받아서 평균 넘는 학생 퍼센트 출력

--- algorithm_0726/4344_평균은넘겠지.py
+++ algorithm_0726/4344_평균은넘겠지.py
@@ -1,5 +1,19 @@
 def over_average(c):
-    count = 0
+    for i in range(c):
+        score_list = list(map(int,input().split()))
+        avg = sum(score_list[1:])/score_list[0]
+        count = 0
+
+        for score in score_list[1:]:
+            if score > avg:
+                count += 1
+        over_average_per = (count/score_list[0])*100
+        print(f'{over_average_per:.3f}%')
 
 c = int(input())
 over_average(c)
+
+# map() 함수로 input 받으면 입력되는 만큼 매칭
+# list() 리스트 형태로 저장
+# [:] 리스트 슬라이스
+# f string 에서 소수점 표현
